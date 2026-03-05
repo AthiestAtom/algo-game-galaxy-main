141d34dec0ca22fe55867580d3376678a8ce1bd9
Jashan Bansal <jashanbansal1101@gmail.com>
2026-03-05 17:22:34 +0530

    Fix base path and add 404 handler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/algo-game-galaxy-main/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
